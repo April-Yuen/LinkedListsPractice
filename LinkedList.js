5cbbe1660cfb1afcc08c5a7b1244c97ed30d14d5
April Yuen <april.you@gmail.com>
2023-02-26 20:27:07 -0500

    Finished the linked list practice.

--- LinkedList.js
+++ LinkedList.js
@@ -20,6 +20,32 @@ class LinkedList{
         return current
     }
 
+    removeHead(){
+        this.head = this.head.next
+        this.length--
+    }
+
+    insertAtIndex(index, value) {
+        if(index === 0) return this.insertAtHead(value)
+
+        const prev = this.getByIndex(index -1)
+        if(prev == null) return null
+
+        prev.next = new LinkedListNode(value, prev.next)
+        this.length++
+    }
+
+    removeAtIndex(index){
+        if(index === 0) return this.removeHead()
+
+        const prev = this.getByIndex(index -1)
+        if(prev == null) return null
+
+        prev.next = prev.next.next
+        this.length--
+    }
+    
+
     print() {
         let output = ''
         let current = this.head
--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const LinkedList = require('./LinkedList')
 
 const ll = LinkedList.fromValues(10, 20, 30, 40)
 ll.print()
-console.log(ll.getByIndex(2).value)
+ll.removeAtIndex(2)
+ll.print()
 
-console.log()
